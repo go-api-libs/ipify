bddbb0a7f1b75c5cfeb90c4699c59003c7f0476f
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-12-22 13:03:28 +0100

    regenerate

--- internal/probe/probe.go
+++ internal/probe/probe.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"net/http"
+
 	"gopkg.in/dnaeon/go-vcr.v3/cassette"
 )
 
 // probe calls the API server to check what we can do
 func probe() error {
-	// define http calls here, e.g.: http.Get(defaultServerURL + "my-endpoint")
-	return nil
+	// define http calls here, e.g.:
+	_, err := http.Get("https://api.ipify.org?format=json")
+	return err
 }
 
 // mask any secrets the API might return, e.g. in the response body
